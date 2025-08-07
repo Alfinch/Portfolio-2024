56974f85bb8af1ffaf7f139f4f622845385cc5e9
Alfinch <alfiewoodland@gmail.com>
2025-08-07 09:27:49 +0100

    Attempt to deploy staticwebapp.config.json

--- next.config.js
+++ next.config.js
@@ -2,19 +2,6 @@
 
 const nextConfig = {
   output: "standalone",
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `default-src 'self'; script-src 'report-sample' 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'report-sample' 'self'; object-src 'none'; base-uri 'self'; connect-src 'self' https://api.alfiewoodland.com; font-src 'self'; frame-src 'self'; img-src 'self' https://alfiewoodlandmedia.blob.core.windows.net; manifest-src 'self'; media-src 'self'; worker-src 'none';`,
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
